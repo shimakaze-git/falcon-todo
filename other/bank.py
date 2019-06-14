ede343e17cd20ae12c007d866c8cd02aa8747530
koshima-shimakaze <koshima@jobhub.jp>
2019-06-14 10:42:46 +0900

    feat(bother/bank.py): 口座登録のモッククラスを作成

---
+++ other/bank.py
@@ -0,0 +1,21 @@
+# enhanceArray
+# depositing 預金
+# withdrawing 引き出す
+# balanceOfAccount 講座のお金を表示
+# outputAllDeposits
+# outputAllWithdraws
+# outputToTextFile
+
+
+class Account:
+    def __init__(self):
+        pass
+
+    def depositing(self):
+        pass
+
+    def with_drawing(self):
+        pass
+
+    def balance_of_account(self):
+        pass
